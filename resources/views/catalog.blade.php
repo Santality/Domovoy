b7af759edfc8a0fbf7b6d9394acab2a21f8f3385
Santality <saintly231@gmail.com>
2023-12-06 12:13:53 +0500

    catalog

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -11,6 +11,62 @@
 </head>
 <body>
     <x-header></x-header>
+    <div class="container">
+        <h2 class="mt-3 mb-3">Каталог</h2>
+        <div class="d-flex justify-content-between row row-cols-1 row-cols-md-2">
+            <div class="col-md-3">
+                <div class="catalog-filter-block">
+                    <h2 class="catalog-filter-block-title">Тип</h2>
+                    <h2 class="catalog-filter-block-title">Недвижимость</h2>
+                    <h2 class="catalog-filter-block-title">Количество комнат</h2>
+                </div>
+            </div>
+            <div class="col-md-9">
+                <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-3">
+                    <div class="col">
+                          <div class="cstm-card-index">
+                            <a href="/post"><img src="/images/hom.jpg" alt=""></a>
+                            <a href="/post"><p>Дом 82 м² на участке 6,3 сот.</p></a>
+                            <a href="/post"><p class="cstm-card-index-cost">5200000 ₽</p></a>
+                            <a href="/post"><p>д. Блохино</p></a>
+                          </div>
+                    </div>
+                    <div class="col">
+                        <div class="cstm-card-index">
+                          <a href="/post"><img src="/images/hom.jpg" alt=""></a>
+                          <a href="/post"><p>Дом 82 м² на участке 6,3 сот.</p></a>
+                          <a href="/post"><p class="cstm-card-index-cost">5200000 ₽</p></a>
+                          <a href="/post"><p>д. Блохино</p></a>
+                        </div>
+                  </div>
+                  <div class="col">
+                    <div class="cstm-card-index">
+                      <a href="/post"><img src="/images/hom.jpg" alt=""></a>
+                      <a href="/post"><p>Дом 82 м² на участке 6,3 сот.</p></a>
+                      <a href="/post"><p class="cstm-card-index-cost">5200000 ₽</p></a>
+                      <a href="/post"><p>д. Блохино</p></a>
+                    </div>
+              </div>
+              <div class="col">
+                <div class="cstm-card-index">
+                  <a href="/post"><img src="/images/hom.jpg" alt=""></a>
+                  <a href="/post"><p>Дом 82 м² на участке 6,3 сот.</p></a>
+                  <a href="/post"><p class="cstm-card-index-cost">5200000 ₽</p></a>
+                  <a href="/post"><p>д. Блохино</p></a>
+                </div>
+          </div>
+          <div class="col">
+                          <div class="cstm-card-index">
+                            <a href="/post"><img src="/images/hom.jpg" alt=""></a>
+                            <a href="/post"><p>Дом 82 м² на участке 6,3 сот.</p></a>
+                            <a href="/post"><p class="cstm-card-index-cost">5200000 ₽</p></a>
+                            <a href="/post"><p>д. Блохино</p></a>
+                          </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <x-footer></x-footer>
 </body>
 </html>
